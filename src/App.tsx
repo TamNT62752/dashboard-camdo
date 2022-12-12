1f71139b776b724737c6a51704b775335b0bbc5d
Tam Nguyen <67701889+TamNT62752@users.noreply.github.com>
2022-12-12 10:32:59 +0000

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <h1 className='underline text-3xl'>App</h1>
   );
 }
 
