0130275fe5fcb772daedb645d0f50c9804431ef6
Tam Nguyen <67701889+TamNT62752@users.noreply.github.com>
2022-12-12 22:17:13 +0700

    Finish page setup

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+export { default as Button } from './Button';
+export { default as Theme } from './Theme';
+export { default as Sidebar } from './Sidebar';
+
+// eslint-disable-next-line import/no-cycle
+export { default as Navbar } from './Navbar';
+export { default as Notification } from './Notification';
+export { default as UserProfile } from './UserProfile';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
